fcec4d98e94410c23fe21d5bcca16aa52a19ccc8
senora <senorariapardede24@gmail.com>
2019-10-13 20:07:09 +0700

    Percobaan

---
+++ minggu-6/percobaan/latihan7.php
@@ -0,0 +1,34 @@
+<?php
+	//Kalkulator Latihan2
+	$a = 15;
+	$b = 5;
+	echo "bilangan 1 = $a"; echo "\n";
+	echo "bilangan 2 = $b"; echo "\n";
+	echo "Berikut hasil perhitungan dari setiap operator";
+	echo "\n";
+	
+	    echo "\n PENJUMLAHAN \n";
+	    $c=$a+$b;
+	    echo "operator = + \n";
+	    echo "hasilnya : $a+$b=$c \n";
+	    
+	    echo "\n PENGURANGAN \n";
+	    $d=$a-$b;
+	    echo "operator = - \n";
+	    echo "hasilnya : $a-$b=$d \n";
+	    
+	    echo "\n PERKALIAN \n";
+	    $e=$a*$b;
+	    echo "operator = * \n";
+	    echo "hasilnya : $a-$b=$e \n";
+	    
+	    echo "\n PEMBAGIAN \n";
+	    $f=$a/$b;
+	    echo "operator = / \n";
+	    echo "hasilnya : $a/$b=$f \n";
+	    
+	    echo "\n MODULUS \n";
+	    $g=$a%$b;
+	    echo "operator = % \n";
+	    echo "hasilnya : $a%$b=$g \n";
+?>
